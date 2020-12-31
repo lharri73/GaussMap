24f3f57346bdf290be0be0a6ecc8b1d5d153f254
Landon Harris <lharri73@vols.utk.edu>
2020-12-31 14:05:20 -0500

    cleanup and restructure
    - added instructions to readme

--- README.md
+++ README.md
@@ -3,6 +3,40 @@
 ## Requirements
 - cmake >=3.17
 - pybind11 >=2.2 (installed with apt is easier)
+- CUDA (tested on 11.2)
 
 ## Installation
 Installation is simplified with python. Simply run `python setup.py install` and dependencies will be installed and configured. 
+
+## Getting Started
+1. Follow the install steps to setup the python library
+
+1. Run CenterTrack on all splits of the nuscenes dataset. 
+
+   These will be pickled together to allow for faster loading later. If you don't need all of them 
+   (for example, only the test for evaluation, then you can only run the test set)
+
+
+## Running CenterTrack
+A couple of changes have been made to centertrack to allow for it to run on NuScenes. 
+
+These changes can be found [here](https://github.com/lharri73/CenterTrack-Nuscenes).
+
+This requires PyTorch version 1.4.0, torchvision 0.5.0, and Cuda <= 10.2
+
+1. Follow the setup steps in the [centertrack repository](https://github.com/xingyizhou/CenterTrack)
+
+    These are mostly in readme/INSTALL.md
+
+1. Run the `convert_nuscenes` script. You'll have to adjust the DATA_ROOT at the top of this file
+
+1. Create an alias in the root of your nuscenes directory called `anotations` that points to the 
+   directory containing the `.json` files just created.
+
+1. Download the pretrained model called `nuScenes_3Ddetection_e140` listed in `readme/MODEL_ZOO.md`.
+   Place this in the `models` folder.
+
+2. Run the following script, adjusting dataset split for each split of the dataset you need
+```bash
+python test.py ddd --exp_id nusc_det_full --load_model ../models/nuScenes_3Ddetection_e140.pth --dataset nuscenes --dataset_version mini-train
+```
---
+++ scripts/genCenterTrack.py
@@ -0,0 +1,53 @@
+import os
+from nuscenes.eval.common.loaders import load_prediction
+from nuscenes.eval.detection.data_classes import DetectionBox
+from nuscenes.nuscenes import NuScenes
+from tqdm import tqdm
+
+import pickle
+
+class_name = {
+      'car': 0.0, 'truck': 1.0, 'bus': 2.0, 'trailer': 3.0, 
+      'construction_vehicle': 4.0, 'pedestrian': 5.0, 'motorcycle': 6.0, 'bicycle': 7.0,
+      'traffic_cone': 8.0, 'barrier': 9.0, 'radar': 10.0}
+
+SPLIT_TO_VER={'val': 'v1.0-trainval', 'train': 'v1.0-trainval', 'test': 'v1.0-test', 'mini-val': 'v1.0-mini', 'mini-train': 'v1.0-mini'}
+
+def main():
+    if os.path.exists('results/CenterTrack/parsed.pkl'):
+        print("Already parsed")
+        exit()
+    
+    parsedList = {}
+    for split in tqdm(SPLIT_TO_VER, position=0):
+        parsedList.update({split: parseVersion(SPLIT_TO_VER[split], split)})
+
+    with open('results/CenterTrack/parsed.pkl', 'wb') as f:
+        print("writing to pickle")
+        pickle.dump(parsedList, f)
+
+def parseVersion(version, split):
+    if not os.path.exists("results/CenterTrack/{}.json".format(split)):
+        print("could not find 'results/CenterTrack/{}.json'...skipping".format(split))
+        return None
+    nusc = NuScenes(version=version, dataroot='/DATA/datasets/nuscenes', verbose=True)
+    camBoxes, meta = load_prediction("results/CenterTrack/{}.json".format(split), 500, DetectionBox, verbose=True)
+    objs = {}
+    for sample, dic in tqdm(camBoxes.boxes.items(), position=1):
+        sampleList = []
+        
+        s_record = nusc.get('sample', sample)
+        sd_record = nusc.get('sample_data', s_record['data']['CAM_FRONT'])
+        pose_rec = nusc.get('ego_pose', sd_record['ego_pose_token'])
+
+        for item in dic:
+            item = global_to_vehicle(item, pose_rec, True)
+            if item.detection_score < .2: continue
+            curList = [item.translation[0], item.translation[1], class_name[item.detection_name]]
+            sampleList.append(curList)
+        
+        objs.update({sample: sampleList})
+    return objs
+
+if __name__ == "__main__":
+    main()
